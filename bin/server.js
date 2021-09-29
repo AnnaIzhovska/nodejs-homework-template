9ddaf5b62288111f970e17fbf3959b4537b731e1
Anna <72921518+AnnaIzhovska@users.noreply.github.com>
2021-09-29 10:45:35 +0200

    In process

--- bin/server.js
+++ bin/server.js
@@ -1,12 +1,31 @@
 const mongoose = require('mongoose')
 const dotenv = require('dotenv')
 dotenv.config()
+mongoose.Promise = global.Promise
 
 const app = require('../app')
 
+app.use(
+  session({
+    secret: 'secret-word',
+    key: 'session-key',
+    cookie: {
+      path: '/',
+      httpOnly: true,
+      maxAge: null,
+    },
+    saveUninitialized: false,
+    resave: false,
+  }),
+)
+require('./config/config-passport')
+app.use(passport.initialize())
+app.use(passport.session())
+
 mongoose.connect(DB_HOST, {
   useNewUrlParser: true,
-  useUnifiedTopology: true
+  useUnifiedTopology: true,
+  useCreateIndex: true,
 })
 .then(() => app.listen(PORT))
 .catch(error => {
